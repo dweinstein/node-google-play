6aa9f80345a646b54cf74a843167bd0d05d22374
David Weinstein <dweinst@insitusec.com>
2014-10-31 15:03:08 -0400

    add bulkDetails example

---
+++ examples/bulkDetails.js
@@ -0,0 +1,23 @@
+var GooglePlayAPI = require('../lib/api').GooglePlayAPI;
+
+var use_cache = true;
+var debug = false;
+
+function getBulkDetails(pkgs) {
+  var api = GooglePlayAPI(
+    process.env.GOOGLE_LOGIN, process.env.GOOGLE_PASSWORD,
+    process.env.ANDROID_ID,
+    use_cache,
+    debug
+  );
+
+  return api.login()
+  .then(function() {
+    api.bulkDetails(pkgs).then(function (res) {
+      console.log('%j', res);
+    });
+  });
+}
+
+getBulkDetails(['com.viber.voip', 'air.WatchESPN']);
+
