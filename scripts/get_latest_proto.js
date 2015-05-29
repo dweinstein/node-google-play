1a648d91cee3559dc741be2f2b31a9fcd84269a1
David Weinstein <dweinst@insitusec.com>
2015-05-29 17:05:51 -0400

    bump proto ver

--- scripts/get_latest_proto.js
+++ scripts/get_latest_proto.js
@@ -5,9 +5,14 @@ var split = require('split');
 var through = require('through2').obj;
 
 var file = fs.createWriteStream(__dirname+'/../lib/data/googleplay.proto');
-var url = "https://raw.githubusercontent.com/dweinstein/google-play-proto/v1.0.0/googleplay.proto";
+var url = "https://raw.githubusercontent.com/dweinstein/google-play-proto/v1.0.3/googleplay.proto";
 
 var request = https.get(url, function(response) {
+  if (response.statusCode != 200) {
+    console.error(url);
+    console.error(response.statusMessage);
+    process.exit(1);
+  }
   response
   .pipe(split())
   .pipe(through(function (chunk, enc, cb) {
