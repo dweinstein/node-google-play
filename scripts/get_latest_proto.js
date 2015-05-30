e1a9a6812d5ecfd3c8faf4a63e522c70d9f672b6
David Weinstein <dweinst@insitusec.com>
2015-05-29 23:03:22 -0400

    refactor to get two protos

--- scripts/get_latest_proto.js
+++ scripts/get_latest_proto.js
@@ -3,21 +3,77 @@ var https = require('https');
 var fs = require('fs');
 var split = require('split');
 var through = require('through2').obj;
+var fmt = require('util').format;
 
 var file = fs.createWriteStream(__dirname+'/../lib/data/googleplay.proto');
-var url = "https://raw.githubusercontent.com/dweinstein/google-play-proto/v1.0.3/googleplay.proto";
+var BASE_URL = "https://raw.githubusercontent.com/dweinstein/google-play-proto/%s/%s";
+var VER = 'v1.0.3';
 
-var request = https.get(url, function(response) {
-  if (response.statusCode != 200) {
-    console.error(url);
-    console.error(response.statusMessage);
+function bytesToString(chunk, enc, cb) {
+  process.nextTick(function() {
+    cb(null, chunk.replace(/bytes/,'string')+'\n');
+  });
+}
+
+function unsplit(chunk, enc, cb) {
+  process.nextTick(function() {
+    return cb(null, chunk+'\n');
+  });
+}
+
+function rel(path) {
+  return __dirname+path;
+}
+
+function urlForVerPath(ver, path) {
+  return fmt(BASE_URL, ver, path);
+}
+
+var files = [
+  {
+    path: rel('/../lib/data/googleplay.proto'),
+    url: urlForVerPath(VER, '/googleplay.proto'),
+    through: bytesToString
+  },
+  {
+    path: rel('/../lib/data/checkin.proto'),
+    url: urlForVerPath(VER, '/checkin/checkin_merged.proto'),
+    through: unsplit
+  }
+];
+
+function done(err) {
+  if (err) {
+    console.log(err);
     process.exit(1);
   }
-  response
-  .pipe(split())
-  .pipe(through(function (chunk, enc, cb) {
-    cb(null, chunk.replace(/bytes/,'string')+'\n');
-  }))
-  .pipe(file);
-});
+}
+
+var count = files.length;
+
+if (count > 0) {
+  files.forEach(function (info) {
+    var url = info.url;
+    var path = info.path;
+    var middle = info.through;
+
+    var request = https.get(url, function(response) {
+      if (response.statusCode != 200) {
+        return done(new Error(fmt('url %s failed, message: %s', url, response.statusMessage)));
+      }
+
+      var file = fs.createWriteStream(path);
+
+      response
+      .pipe(split())
+      .pipe(through(middle))
+      .pipe(file, {end: true});
+
+      response.once('end', function() {
+        if (--count <= 0) done();
+      });
+
+    });
+  });
+}
 
