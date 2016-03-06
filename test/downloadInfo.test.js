0316a53601d377ba50e51c8be215a2755dc26b72
David Weinstein <dweinst@insitusec.com>
2016-03-06 13:46:56 -0500

    Add `AppNotFreeError` for unimpl. paid requests
    - Error has `AppNotFreeError.name` and `AppNotFreeError.price`
      (the offer `formattedAmount` string)

--- test/downloadInfo.test.js
+++ test/downloadInfo.test.js
@@ -1,12 +1,22 @@
 var api = require('./api');
-
 var test = require('tape');
+var AppNotFreeError = require('../lib/errors').AppNotFreeError;
 
-test.skip('downloadInfo api', function (t) {
+test('downloadInfo api', function (t) {
+  t.plan(7);
   api.downloadInfo('com.viber.voip', 37, function (err, res) {
     t.notOk(err, 'no error');
     t.ok(res, 'returned results');
-    t.end();
   });
+  setTimeout(function () {
+    api.downloadInfo('com.mojang.minecraftpe', 740140009, function (err, res) {
+      t.ok(err, 'error expected');
+      t.ok(err instanceof AppNotFreeError, 'error instanceof AppNotFreeError');
+      t.ok(err.name === 'AppNotFreeError', 'error.name is AppNotFreeError');
+      t.ok(err.price, 'error has price');
+      t.notOk(res, 'no results');
+      t.comment('price: ' + err.price);
+    });
+  }, 1500);
 });
 
