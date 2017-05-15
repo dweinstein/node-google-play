4486877545c84420717ace0c990998a26413f999
David Weinstein <dweinst@insitusec.com>
2017-05-14 22:03:44 -0400

    Tweak search

--- test/search.test.js
+++ test/search.test.js
@@ -5,7 +5,7 @@ var test = require('tap').test;
 test('search api', function (t) {
   t.plan(2);
   api.search('viber', function (err, res) {
-    t.notOk(err, 'no error');
+    t.false(err, 'no error');
     t.ok(res, 'returned results');
   });
 });
